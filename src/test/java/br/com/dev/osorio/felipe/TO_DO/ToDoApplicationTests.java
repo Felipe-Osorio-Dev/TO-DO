e6dae81b5669b98b3cc53185aa5276280dc1a7e8
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2025-10-30 02:07:18 -0300

    Initial commit

---
+++ src/test/java/br/com/dev/osorio/felipe/TO_DO/ToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dev.osorio.felipe.TO_DO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
