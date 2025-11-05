462b83e4e6569bfdaf68bfebfbd9d3ed0787cae5
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2025-11-05 01:20:39 -0300

    feat: Added method that deletes an item from the database by id

--- src/main/java/br/com/dev/osorio/felipe/TO_DO/service/TaskService.java
+++ src/main/java/br/com/dev/osorio/felipe/TO_DO/service/TaskService.java
@@ -2,10 +2,13 @@ package br.com.dev.osorio.felipe.TO_DO.service;
 
 import br.com.dev.osorio.felipe.TO_DO.dto.request.CreateTaskRequest;
 import br.com.dev.osorio.felipe.TO_DO.dto.response.CreateTaskResponse;
+import br.com.dev.osorio.felipe.TO_DO.entity.TaskEntity;
 import br.com.dev.osorio.felipe.TO_DO.mapper.TaskMapper;
 import br.com.dev.osorio.felipe.TO_DO.repository.TaskRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class TaskService {
@@ -23,5 +26,16 @@ public class TaskService {
         var task = taskRepository.saveAndFlush(taskMapper.toTask(request));
 
         return new CreateTaskResponse(task.getId(), task.getName());
+    }
+
+    @Transactional
+    public void deleteTask(Long id) {
+        Optional<TaskEntity> task = taskRepository.findById(id);
+
+        if (!task.isPresent()) {
+            throw new RuntimeException("Task not found");
+        }
+
+        taskRepository.deleteById(task.get().getId());
     }
 }
