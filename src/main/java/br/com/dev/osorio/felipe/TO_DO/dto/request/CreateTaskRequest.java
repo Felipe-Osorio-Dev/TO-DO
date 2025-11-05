d42d5766b7e321a5f4c851fe487b549212d13732
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2025-11-05 01:13:36 -0300

    feat: Created DTOs for requests and responses in the task creation part

---
+++ src/main/java/br/com/dev/osorio/felipe/TO_DO/dto/request/CreateTaskRequest.java
@@ -0,0 +1,14 @@
+package br.com.dev.osorio.felipe.TO_DO.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDate;
+
+public record CreateTaskRequest(
+        @NotBlank(message = "Nome Obrigatório!!")
+        String name,
+        String description,
+        LocalDate endTime,
+        String status
+) {
+}
---
+++ src/main/java/br/com/dev/osorio/felipe/TO_DO/dto/response/CreateTaskResponse.java
@@ -0,0 +1,4 @@
+package br.com.dev.osorio.felipe.TO_DO.dto.response;
+
+public record CreateTaskResponse(Long id, String name) {
+}
