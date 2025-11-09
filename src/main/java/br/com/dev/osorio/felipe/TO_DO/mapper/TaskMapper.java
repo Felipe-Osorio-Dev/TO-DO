a8e385838229517f3850af98ccd0f6b70d5b5fc7
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2025-11-09 00:31:34 -0300

    feat: Created a mapper to map entities and DTOs

--- src/main/java/br/com/dev/osorio/felipe/TO_DO/mapper/TaskMapper.java
+++ src/main/java/br/com/dev/osorio/felipe/TO_DO/mapper/TaskMapper.java
@@ -1,13 +1,22 @@
 package br.com.dev.osorio.felipe.TO_DO.mapper;
 
-import br.com.dev.osorio.felipe.TO_DO.dto.request.CreateTaskRequest;
+import br.com.dev.osorio.felipe.TO_DO.dto.TaskDTO;
+import br.com.dev.osorio.felipe.TO_DO.dto.request.RegisterRequest;
 import br.com.dev.osorio.felipe.TO_DO.entity.TaskEntity;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface TaskMapper {
 
     @Mapping(target = "id", ignore = true)
-    TaskEntity toTask(CreateTaskRequest request);
+    TaskEntity toTask(RegisterRequest request);
+
+    TaskEntity toTask(TaskDTO request);
+
+    TaskDTO toTaskDTO(TaskEntity entity);
+
+    RegisterRequest toRegisterRequest(TaskEntity entity);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    TaskEntity updateTask(TaskDTO request, @MappingTarget TaskEntity entity);
 }
