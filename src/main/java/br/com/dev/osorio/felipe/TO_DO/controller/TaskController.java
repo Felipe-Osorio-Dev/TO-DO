f803aa9af4c65fa5cb535bbf7a13e07dbde76ca3
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2025-11-10 22:20:54 -0300

    refactor: refactor method createTask and add mapper to convert entity and dto

--- src/main/java/br/com/dev/osorio/felipe/TO_DO/controller/TaskController.java
+++ src/main/java/br/com/dev/osorio/felipe/TO_DO/controller/TaskController.java
@@ -3,6 +3,8 @@ package br.com.dev.osorio.felipe.TO_DO.controller;
 import br.com.dev.osorio.felipe.TO_DO.dto.TaskDTO;
 import br.com.dev.osorio.felipe.TO_DO.dto.request.RegisterRequest;
 import br.com.dev.osorio.felipe.TO_DO.dto.response.RegisterResponse;
+import br.com.dev.osorio.felipe.TO_DO.entity.TaskEntity;
+import br.com.dev.osorio.felipe.TO_DO.mapper.TaskMapper;
 import br.com.dev.osorio.felipe.TO_DO.service.TaskService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -14,9 +16,11 @@ import org.springframework.web.bind.annotation.*;
 public class TaskController {
 
     private final TaskService taskService;
+    private final TaskMapper taskMapper;
 
-    public TaskController(TaskService taskService) {
+    public TaskController(TaskService taskService, TaskMapper taskMapper) {
         this.taskService = taskService;
+        this.taskMapper = taskMapper;
     }
 
     @GetMapping
@@ -28,7 +32,10 @@ public class TaskController {
 
     @PostMapping("/create")
     public ResponseEntity<RegisterResponse> createTask(@Valid @RequestBody RegisterRequest request) {
-        var response = taskService.registerTask(request);
+        TaskEntity entity = taskMapper.fromRegisterRequest(request);
+        TaskEntity taskSaved = taskService.registerTask(entity);
+
+        RegisterResponse response = new RegisterResponse(taskSaved.getId(), taskSaved.getName());
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(response);
