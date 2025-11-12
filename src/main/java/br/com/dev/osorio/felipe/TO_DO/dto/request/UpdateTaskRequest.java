c484bc69e314afcda0fb237c961369042ee72fea
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2025-11-11 22:19:45 -0300

    feat: create request and response for update tasks

---
+++ src/main/java/br/com/dev/osorio/felipe/TO_DO/dto/request/UpdateTaskRequest.java
@@ -0,0 +1,15 @@
+package br.com.dev.osorio.felipe.TO_DO.dto.request;
+
+import br.com.dev.osorio.felipe.TO_DO.util.StatusTask;
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDate;
+
+public record UpdateTaskRequest(
+        Long id,
+        @NotBlank(message = "Nome Obrigatório!!") String name,
+        @NotBlank(message = "Descrição Obrigatória!!") String description,
+        @NotBlank(message = "Data de Expiração Obrigatória!!!") LocalDate endTime,
+        @NotBlank(message = "Status Obrigatório!!!") StatusTask status
+) {
+}
