4e4b66f2229cd38413a23d98300859b3bddc5537
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2025-11-12 22:53:30 -0300

    refactor: renamed constants

--- src/main/java/br/com/dev/osorio/felipe/TO_DO/util/StatusTask.java
+++ src/main/java/br/com/dev/osorio/felipe/TO_DO/util/StatusTask.java
@@ -1,17 +1,6 @@
 package br.com.dev.osorio.felipe.TO_DO.util;
 
 public enum StatusTask {
-    INIT("Tarefa a ser Iniciada!!"),
-    RUNNING("Tarefa em Andamento!!"),
-    FINISHED("Tarefa Finalizada!!");
-
-    private String description;
-
-    StatusTask(String description) {
-        this.description = description;
-    }
-
-    public String getDescription() {
-        return description;
-    }
+    INICIADA,
+    FINALIZADA
 }
