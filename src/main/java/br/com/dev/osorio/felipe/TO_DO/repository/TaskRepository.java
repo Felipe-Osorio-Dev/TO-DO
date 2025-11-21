a769d409d695f7545d997821add89557287405f5
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2025-11-21 01:18:37 -0300

    feat: add new query findByName

--- src/main/java/br/com/dev/osorio/felipe/TO_DO/repository/TaskRepository.java
+++ src/main/java/br/com/dev/osorio/felipe/TO_DO/repository/TaskRepository.java
@@ -4,6 +4,10 @@ import br.com.dev.osorio.felipe.TO_DO.entity.TaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
+
+    Optional<TaskEntity> findByName(String name);
 }
